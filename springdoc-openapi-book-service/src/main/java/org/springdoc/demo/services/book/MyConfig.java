b50e35d92661ff3d2a2972b92da2231a65680ecb
Carl Magnuson <carl.magnuson@mckesson.com>
2022-05-27 10:49:09 -0500

    Add MultipleOpenApiWebMvcResource bean

--- springdoc-openapi-book-service/src/main/java/org/springdoc/demo/services/book/MyConfig.java
+++ springdoc-openapi-book-service/src/main/java/org/springdoc/demo/services/book/MyConfig.java
@@ -1,12 +1,38 @@
 package org.springdoc.demo.services.book;
 
+import org.springdoc.core.AbstractRequestService;
+import org.springdoc.core.GenericResponseService;
 import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.OpenAPIService;
+import org.springdoc.core.OperationService;
+import org.springdoc.core.SpringDocConfigProperties;
+import org.springdoc.core.SpringDocProviders;
+import org.springdoc.webmvc.api.MultipleOpenApiWebMvcResource;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+import java.util.List;
 
 @Configuration
 public class MyConfig {
+
+
+    @Bean
+    @Lazy(false)
+    MultipleOpenApiWebMvcResource multipleOpenApiResource(List<GroupedOpenApi> groupedOpenApis,
+                                                          ObjectFactory<OpenAPIService> defaultOpenAPIBuilder, AbstractRequestService requestBuilder,
+                                                          GenericResponseService responseBuilder, OperationService operationParser,
+                                                          SpringDocConfigProperties springDocConfigProperties,
+                                                          SpringDocProviders springDocProviders) {
+        return new MultipleOpenApiWebMvcResource(groupedOpenApis,
+                defaultOpenAPIBuilder, requestBuilder,
+                responseBuilder, operationParser,
+                springDocConfigProperties,
+                springDocProviders);
+    }
 
     public ConfigurableListableBeanFactory beanFactory;
 
